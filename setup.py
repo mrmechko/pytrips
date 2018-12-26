1e4515363ae535918d9c7c122295ef6b5ecfb0c7
Rik <rbose@cs.rochester.edu>
2018-12-26 07:34:33 +0530

    pytrips

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytrips",
+    version="0.0.1",
+    author="Rik Bose",
+    author_email="rbose@cs.rochester.edu",
+    description="A simple python package for accessing the trips ontology and lexicon",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrmechko/simpletrips",
+    packages=find_namespace_packages(include=["pytrips.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

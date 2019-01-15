4a65296775348de1a8266148f0202ded35f30162
Rik <rbose@cs.rochester.edu>
2019-01-14 23:17:30 -0500

    do you know how much I hate python and setuptools and all that jazz? The answer is: a lot.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mrmechko/pytrips",
-    packages=find_namespace_packages(include=["pytrips.*"]),
+    packages=find_packages(exclude=["test"]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

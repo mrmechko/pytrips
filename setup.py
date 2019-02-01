84be52a07b30700ee58501c49758e77dcb2983fe
Rik <rbose@cs.rochester.edu>
2019-02-01 16:03:47 -0500

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ class EnsureWordnet(install):
 if __name__ == '__main__':
     setup(
         name="pytrips",
-        version="0.0.1",
+        version="0.0.2",
         author="Rik Bose",
         author_email="rbose@cs.rochester.edu",
         description="A simple python package for accessing the trips ontology and lexicon",

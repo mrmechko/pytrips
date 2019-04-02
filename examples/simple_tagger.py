0bd9d47c35f57eb50b9fa9f4a53000bacef0a8db
Rik <rbose@cs.rochester.edu>
2019-04-02 16:26:51 -0400

    simple tagger works now

--- examples/simple_tagger.py
+++ examples/simple_tagger.py
@@ -1,22 +1,26 @@
-from pytrips.ontology import load as load_ontology
-from pytrips.helpers import Normalize
-
 # turn off all the logging
 import logging
-logging.basicConfig()
-ont = load_ontology()
+logging.basicConfig(level=logging.CRITICAL)
 
+from pytrips.ontology import load as load_ontology
+from pytrips.helpers import Normalize
+from pytrips.tools import nlp
+
+ont = load_ontology()
 
 def tag_word(token):
     pos = Normalize.spacy_pos(token.pos_)
     word = token.text.lower()
     lemma = token.lemma_
 
+    if pos not in "nvar": # if the pos is not in wordnet
+        return set()
+
     # word lookup:
     wlookup = ont[("q::"+word, pos)]
     llookup = ont[("q::"+lemma, pos)]
 
-    res += wlookup["lex"] 
+    res = wlookup["lex"] 
     res += wlookup["wn"]
     res += llookup["lex"]
     res += llookup["wn"]
@@ -24,4 +28,5 @@ def tag_word(token):
     return set(res)
 
 def tag_sentence(sentence):
-    return [tag_word(token) for token in nlp(sentence)]
+    sentence = nlp(sentence)
+    return zip([tag_word(token) for token in sentence], sentence)

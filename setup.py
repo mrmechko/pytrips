52a04633f2b5137e7065da7a0caacd1cf4d50ef4
Rik <rbose@cs.rochester.edu>
2020-01-29 05:27:16 -0500

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ base = [
 if __name__ == '__main__':
     setup(
         name="pytrips",
-        version="0.5.0",
+        version="0.5.1",
         author="Rik Bose",
         author_email="rbose@cs.rochester.edu",
         description="A simple python package for accessing the trips ontology and lexicon",
